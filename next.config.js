58acdcd914201c5b93f08ad24ae99b4134d09d15
davidmoltin <56250584+davidmoltin@users.noreply.github.com>
2022-08-26 12:24:52 -0400

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,24 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  images: {
+    domains: ["files-eu.epusercontent.com"],
+  },
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
+module.exports = nextConfig;
