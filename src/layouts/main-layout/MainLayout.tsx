7a15b5c77c3c4dd9022e4a5a6563f4d31c2d20cc
David Stover <david.stover@elasticpath.com>
2022-08-30 09:45:28 -0400

    Update Builder.io, rename files

--- src/layouts/main-layout/MainLayout.tsx
+++ src/layouts/main-layout/MainLayout.tsx
@@ -1,7 +1,7 @@
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import Announcement from "../../components/builder/Announcement";
-import Hero from "../../components/builder/Hero";
+import Home from "../../components/builder/Home";
 
 import type { ReactNode } from "react";
 import type { NavigationNode } from "../../lib/build-site-navigation";
@@ -16,7 +16,7 @@ const MainLayout = ({ nav, children }: IMainLayout): JSX.Element => {
     <>
       <Announcement announce={undefined} />
       <Header nav={nav} />
-      <Hero hero={undefined} /> 
+      <Home Home={undefined} /> 
       {children}
       <Footer />
     </>
